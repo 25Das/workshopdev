91efe8d33d1a04d98e4cafe0e08707fcb193af95
25Das <daniel.raizon@gmail.com>
2020-07-15 16:14:29 -0300

    Organização das pastas e configurações do servidor

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express');
 
 const server = express();
 
-// Criando uma rota /
+// Configurar arquivos estáticos
+server.use(express.static('public'));
+
+// Criando uma rota '/'
 // e captura o pedido do cliente para responder
 server.get('/', (req, res) => {
-	return res.send('Resposta do Servidor');
+	return res.sendFile(`${__dirname}/index.html`);
 });
 
+server.get('/ideias', (req, res) => {
+	return res.sendFile(`${__dirname}/ideias.html`);
+});
 // Servidor ligado na porta 3000
 server.listen(3000);

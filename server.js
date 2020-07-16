74715b2d9f9cdc9529a8d9924d06db72bf73d429
25Das <daniel.raizon@gmail.com>
2020-07-16 14:58:30 -0300

    Integração do Banco de dados com o servidor

--- server.js
+++ server.js
@@ -3,54 +3,6 @@ const express = require('express');
 
 const server = express();
 
-const ideas = [
-	{
-		img: 'https://image.flaticon.com/icons/svg/2729/2729007.svg',
-		title: 'Cursos de Programação',
-		category: 'Estudo',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-		url: 'https://rocketseat.com.br',
-	},
-	{
-		img: 'https://image.flaticon.com/icons/svg/2729/2729005.svg',
-		title: 'Exercícios',
-		category: 'Saúde',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-		url: 'https://rocketseat.com.br',
-	},
-	{
-		img: 'https://image.flaticon.com/icons/svg/2729/2729027.svg',
-		title: 'Meditação',
-		category: 'Mentalidade',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-		url: 'https://rocketseat.com.br',
-	},
-	{
-		img: 'https://image.flaticon.com/icons/svg/2729/2729032.svg',
-		title: 'Karaokê',
-		category: 'Diversão em Família',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-		url: 'https://rocketseat.com.br',
-	},
-	{
-		img: 'https://image.flaticon.com/icons/svg/2729/2729038.svg',
-		title: 'Pintura',
-		category: 'Criatividade',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-		url: 'https://rocketseat.com.br',
-	},
-	{
-		img: 'https://image.flaticon.com/icons/svg/2729/2729048.svg',
-		title: 'Recortes',
-		category: 'Criatividade',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-		url: 'https://rocketseat.com.br',
-	},
-];
-
-// Configurar arquivos estáticos (css, scripts, imagens)
-server.use(express.static('public'));
-
 // Configuração do Nunjucks
 const nunjucks = require('nunjucks');
 
@@ -59,23 +11,81 @@ nunjucks.configure('views', {
 	noCache: true,
 });
 
+const db = require('./db');
+
+// Configurar arquivos estáticos (css, scripts, imagens)
+server.use(express.static('public'));
+
+// Hibilitar uso do req.body
+server.use(express.urlencoded({ extended: true }));
+
 // Criando uma rota '/' e captura o pedido do cliente para responder
 server.get('/', (req, res) => {
-	const reversedIdeas = [...ideas].reverse();
-
-	const lastIdeas = [];
-	for (const idea of reversedIdeas) {
-		if (lastIdeas.length < 2) {
-			lastIdeas.push(idea);
+	// Consultar dados na Tabela
+	db.all(`SELECT * FROM ideas`, (err, rows) => {
+		if (err) {
+			console.log(err);
+			return res.send('Erro no Bando de Dados!');
 		}
-	}
 
-	return res.render('index.html', { ideas: lastIdeas });
+		// console.log(rows);
+
+		const reversedIdeas = [...rows].reverse();
+
+		const lastIdeas = [];
+		for (const idea of reversedIdeas) {
+			if (lastIdeas.length < 2) {
+				lastIdeas.push(idea);
+			}
+		}
+
+		return res.render('index.html', { ideas: lastIdeas });
+	});
 });
 
 server.get('/ideias', (req, res) => {
-	const reversedIdeas = [...ideas].reverse();
-	return res.render('ideias.html', { ideas: reversedIdeas });
+	db.all(`SELECT * FROM ideas`, (err, rows) => {
+		if (err) {
+			console.log(err);
+			return res.send('Erro no Bando de Dados!');
+		}
+
+		// console.log(rows);
+
+		const reversedIdeas = [...rows].reverse();
+		return res.render('ideias.html', { ideas: reversedIdeas });
+	});
+});
+
+server.post('/', (req, res) => {
+	// Inserir dados na Tabela
+	const query = `
+		INSERT INTO ideas(
+			image,
+			title,
+			category,
+			description,
+			link
+		) VALUES (?,?,?,?,?);
+	`;
+
+	const values = [
+		req.body.image,
+		req.body.title,
+		req.body.category,
+		req.body.description,
+		req.body.link,
+	];
+
+	db.run(query, values, (err) => {
+		if (err) {
+			console.log(err);
+			return res.send('Erro no Bando de Dados!');
+		}
+
+		// return console.log(this);
+		return res.redirect('/ideias');
+	});
 });
 // Servidor ligado na porta 3000
 server.listen(3000);

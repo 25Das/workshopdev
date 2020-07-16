4255b7827b06853f0e7801a34da648a336690e79
25Das <daniel.raizon@gmail.com>
2020-07-15 22:00:01 -0300

    Incluído código do nunjuncks e alterações pontuais

--- server.js
+++ server.js
@@ -3,17 +3,79 @@ const express = require('express');
 
 const server = express();
 
-// Configurar arquivos estáticos
+const ideas = [
+	{
+		img: 'https://image.flaticon.com/icons/svg/2729/2729007.svg',
+		title: 'Cursos de Programação',
+		category: 'Estudo',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+		url: 'https://rocketseat.com.br',
+	},
+	{
+		img: 'https://image.flaticon.com/icons/svg/2729/2729005.svg',
+		title: 'Exercícios',
+		category: 'Saúde',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+		url: 'https://rocketseat.com.br',
+	},
+	{
+		img: 'https://image.flaticon.com/icons/svg/2729/2729027.svg',
+		title: 'Meditação',
+		category: 'Mentalidade',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+		url: 'https://rocketseat.com.br',
+	},
+	{
+		img: 'https://image.flaticon.com/icons/svg/2729/2729032.svg',
+		title: 'Karaokê',
+		category: 'Diversão em Família',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+		url: 'https://rocketseat.com.br',
+	},
+	{
+		img: 'https://image.flaticon.com/icons/svg/2729/2729038.svg',
+		title: 'Pintura',
+		category: 'Criatividade',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+		url: 'https://rocketseat.com.br',
+	},
+	{
+		img: 'https://image.flaticon.com/icons/svg/2729/2729048.svg',
+		title: 'Recortes',
+		category: 'Criatividade',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+		url: 'https://rocketseat.com.br',
+	},
+];
+
+// Configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static('public'));
 
-// Criando uma rota '/'
-// e captura o pedido do cliente para responder
+// Configuração do Nunjucks
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+	express: server,
+	noCache: true,
+});
+
+// Criando uma rota '/' e captura o pedido do cliente para responder
 server.get('/', (req, res) => {
-	return res.sendFile(`${__dirname}/index.html`);
+	const reversedIdeas = [...ideas].reverse();
+
+	const lastIdeas = [];
+	for (const idea of reversedIdeas) {
+		if (lastIdeas.length < 2) {
+			lastIdeas.push(idea);
+		}
+	}
+
+	return res.render('index.html', { ideas: lastIdeas });
 });
 
 server.get('/ideias', (req, res) => {
-	return res.sendFile(`${__dirname}/ideias.html`);
+	const reversedIdeas = [...ideas].reverse();
+	return res.render('ideias.html', { ideas: reversedIdeas });
 });
 // Servidor ligado na porta 3000
 server.listen(3000);
